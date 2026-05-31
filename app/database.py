89f0d028466f271bb733292f273bfd2f5bd73f58
luis <ljmm2k2@gmail.com>
2026-05-31 17:47:26 -0400

    refactor: add full database connection

--- app/database.py
+++ app/database.py
@@ -1,13 +1,30 @@
-import os
 import redis
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-def get_redis_client():
-    return redis.from_url(
-        os.getenv("REDIS_URL"), 
-        decode_responses=True
-    )
 
-# global instance so you can import it
-redis_client = get_redis_client()
+
+class RedisClient:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            # Obtenemos las variables del .env
+            host = os.getenv("UPSTASH_REDIS_REST_URL")
+            port = int(os.getenv("UPSTASH_REDIS_PORT", 6379))
+            token = os.getenv("UPSTASH_REDIS_REST_TOKEN")
+
+            # Creamos la instancia (Singleton)
+            cls._instance = redis.Redis(
+                host=host,
+                port=port,
+                password=token,
+                decode_responses=True,
+                ssl=True  # Needed for upstash
+            )
+        return cls._instance
+
+
+# Exporting a single instance for the whole Project
+redis_db = RedisClient()
