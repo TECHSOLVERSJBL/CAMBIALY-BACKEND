eefdb7899ab5288b864f0367c5e53efb18b5b95d
luis <ljmm2k2@gmail.com>
2026-06-07 22:29:40 -0400

    feat: add yadio usdt backup function service

--- app/services.py
+++ app/services.py
@@ -4,6 +4,23 @@ from app.database import redis_client
 
 # Usamos el logger de uvicorn para ver los mensajes en la consola de Docker
 logger = logging.getLogger("uvicorn.error")
+import httpx
+import logging
+
+async def fetch_yadio_rate():
+    """uses USDT/VES rate from Yadio.io as backup."""
+    try:
+        async with httpx.AsyncClient(timeout=5.0) as client:
+            response = await client.get("https://api.yadio.io/exchanges/VES")
+            response.raise_for_status()
+            data = response.json()
+            # Yadio devuelve un objeto con varios exchanges, buscamos 'Binance'
+            rate = data.get("USDT", {}).get("price")
+            return float(rate)
+
+    except Exception as e:
+        logger.error(f"Error fatal obteniendo tasa de Yadio: {e}")
+        return 0.00 # Valor de emergencia "seguro"
 
 def ping_upstash_redis():
     """
